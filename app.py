b0923109cdf02005d8a3d182bee6605f99bf6b7f
UkantJadia <ukantjadia0120@gmail.com>
2023-04-01 10:11:42 +0530

    new app added

--- app.py
+++ app.py
@@ -1,86 +1,71 @@
-import streamlit as st 
+import streamlit as st
 import numpy as np 
-import pandas as pd
-import time
-from PIL import Image 
+from PIL import Image
 
-st.title("Image Editor")
 
-raw = st.file_uploader("Upload a jpg | png image",type=['png','jpg'],accept_multiple_files=False)
-if (raw):
-    with Image.open(raw) as img:
-        img.load()
+col1, col2 = st.columns([0.8,0.2])
 
-    time.sleep(2)
-    gray_img = img.convert('L')
-    np_img = np.array(gray_img)
-    size = np_img.size 
-    shape = np_img.shape
+with col1:
+    st.markdown(""" <style> .font {
+        font-size:35px,font-family:'Copper Black';color:#FF9633;
+    }
+    """,unsafe_allow_html=True)
+    st.markdown('<p class="font"> Upload your photo here...</p>',unsafe_allow_html=True)
+    file = st.file_uploader("",type=['png','jpg','jpeg'])
 
-# def bright(np_img,value):
-#     img_bri = np_img.copy()
-#     for row in range(0,shape[0]):
-#         for col in range(0,shape[1]):
-#             img_bri[row][col] += value  
-#             if img_bri[row][col] > 255:
-#                 img_bri[row][col] = 255
 
-#     img2 = Image.fromarray(img_bri)
-#     return img2
+with col2:
+    st.image()
 
-# def dark(np_img,value):
-#     img_dar = np_img.copy()
-#     for row in range(0,shape[0]):
-#         for col in range(0,shape[1]):
-#             img_dar[row][col] -= value  
-#             if img_dar[row][col] < 0:
-#                 img_dar[row][col] = 0 
+st.sidebar.markdown('<p class="font"> Low Level Image Processing App</p>',unsafe_allow_html=True)
+
+with st.sidebar.expander("About the App"):
+    st.write("""
+        This is a Simple Low level image processing app build with library Pillow, numpy, and streamlit in python. 
+    """)
     
-#     img1 = Image.fromarray(img_dar)
-#     return img1
 
-if (raw):
-    button1 = st.button("let's go...")
-    col1,col2 = st.columns(2)
-    col1.metric("Size of image ",size)
-    col2.metric("Shape of image ",'x'.join(map(str,shape)))
+if file is not None:
+    img = Image.open(file)
 
-    col3,col4 = st.columns(2)
-    with col3:
-        if (st.button("See Gray image")):
-            st.image(gray_img)
+    col1,col2 = st.columns([0.5,0.5])
+    with col1:
+        st.markdown('<p style="text-align:centher;"> Before</p>',unsafe_allow_html=True)
+        st.image(img,width=300)
 
-    with col4: 
-        if (st.button("Image in Array")):
-            st.write(np_img)
-
-
-if (raw):
-    if (st.button("Manipulate the Image")):
-        # value = st.number_input("Pick a number",5,255)
-
-        st.write("Manipulation with Brightness of Image")
-        col1,col2 = st.columns(2)
-        with col1:
-            value = st.slider("pick a number",2,255)
-            st.write("Brightned Image")
-            img_bri = np_img.copy()
-            for row in range(0,shape[0]):
-                for col in range(0,shape[1]):
-                    img_bri[row][col] += value  
-                    if img_bri[row][col] > 255:
-                        img_bri[row][col] = 255
-            img2 = Image.fromarray(img_bri)
-            st.image(img2)
-
-        with col2:
-            st.write("Darken Image")
-            value = st.slider("pick a number",2,255)
-            img_dar = np_img.copy()
-            for row in range(0,shape[0]):
-                for col in range(0,shape[1]):
-                    img_dar[row][col] -= value  
-                    if img_dar[row][col] < 0:
-                        img_dar[row][col] = 0 
-            img1 = Image.fromarray(img_dar)
+    with col2:
+        st.markdown('<p style="text-align:center;"> After</p>',unsafe_allow_html=True)
+        filter = st.sidebar.radio('Play with you Image:',['Original','Gray Image','Edge','Bright','Dark','Contrast'])
+        if filter == 'Gray Image':
+            gray_img = img.convert('L')
+            np_gray = np.array(gray_img)
+            st.image(gray_img,width=300)
+        elif filter == "Bright":
+            gray_img = img.convert('L')
+            np_gray = np.array(gray_img)
+            shape = np_gray.shape
+            img2 = np_gray.copy()
+            slider = st.sidebar.slider("Adjust intensity",2,255,step=2)
+            for row in range(len(np_gray)):
+                for col in range(len(np_gray)):
+                    img2[row][col] += slider
+                    if img2[row][col] > 255:
+                        img2[row][col]  = 255
+            img1 = Image.fromarray(img2)
             st.image(img1)
+            
+        elif filter == "Dark":
+            gray_img = img.convert('L')
+            np_gray = np.array(gray_img)
+            shape = np_gray.shape
+            img2 = np_gray.copy()
+            slider = st.sidebar.slider("Adjust intensity",2,255,step=2)
+            for row in range(len(np_gray)):
+                for col in range(len(np_gray)):
+                    img2[row][col] -= slider
+                    if img2[row][col] < 0:
+                        img2[row][col]  = 0  
+            img1 = Image.fromarray(img2)
+            st.image(img1)
+
+
---
+++ app2.py
@@ -0,0 +1,86 @@
+import streamlit as st 
+import numpy as np 
+import pandas as pd
+import time
+from PIL import Image 
+
+st.title("Image Editor")
+
+raw = st.file_uploader("",type=['png','jpg'],accept_multiple_files=False)
+if (raw):
+    with Image.open(raw) as img:
+        img.load()
+
+    time.sleep(2)
+    gray_img = img.convert('L')
+    np_img = np.array(gray_img)
+    size = np_img.size 
+    shape = np_img.shape
+
+# def bright(np_img,value):
+#     img_bri = np_img.copy()
+#     for row in range(0,shape[0]):
+#         for col in range(0,shape[1]):
+#             img_bri[row][col] += value  
+#             if img_bri[row][col] > 255:
+#                 img_bri[row][col] = 255
+
+#     img2 = Image.fromarray(img_bri)
+#     return img2
+
+# def dark(np_img,value):
+#     img_dar = np_img.copy()
+#     for row in range(0,shape[0]):
+#         for col in range(0,shape[1]):
+#             img_dar[row][col] -= value  
+#             if img_dar[row][col] < 0:
+#                 img_dar[row][col] = 0 
+    
+#     img1 = Image.fromarray(img_dar)
+#     return img1
+
+if (raw):
+    button1 = st.button("let's go...")
+    col1,col2 = st.columns(2)
+    col1.metric("Size of image ",size)
+    col2.metric("Shape of image ",'x'.join(map(str,shape)))
+
+    col3,col4 = st.columns(2)
+    with col3:
+        if (st.button("See Gray image")):
+            st.image(gray_img)
+
+    with col4: 
+        if (st.button("Image in Array")):
+            st.write(np_img)
+
+
+if (raw):
+    if (st.button("Manipulate the Image")):
+        # value = st.number_input("Pick a number",5,255)
+
+        st.write("Manipulation with Brightness of Image")
+        col1,col2 = st.columns(2)
+        with col1:
+            value = st.slider("pick a number",2,255)
+            st.write("Brightned Image")
+            img_bri = np_img.copy()
+            for row in range(0,shape[0]):
+                for col in range(0,shape[1]):
+                    img_bri[row][col] += value  
+                    if img_bri[row][col] > 255:
+                        img_bri[row][col] = 255
+            img2 = Image.fromarray(img_bri)
+            st.image(img2)
+
+        with col2:
+            st.write("Darken Image")
+            value2 = st.slider("pick a number",2,255)
+            img_dar = np_img.copy()
+            for row in range(0,shape[0]):
+                for col in range(0,shape[1]):
+                    img_dar[row][col] -= value2  
+                    if img_dar[row][col] < 0:
+                        img_dar[row][col] = 0 
+            img1 = Image.fromarray(img_dar)
+            st.image(img1)
